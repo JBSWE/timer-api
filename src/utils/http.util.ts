873f50457a9d9037635cc7c433ea48069f69b7ff
JBSWE <jackburke1993@gmail.com>
2022-07-21 17:04:58 +0200

    feat: add http utility functions

--- src/utils/http.util.ts
+++ src/utils/http.util.ts
@@ -1,4 +1,29 @@
-import type { APIGatewayProxyEvent } from 'aws-lambda'
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { Timer } from "../models/timer.model";
+
+export function makeAcceptedHttpResponse(result: Timer): APIGatewayProxyResult {
+  return {
+    statusCode: 202,
+    body: JSON.stringify({id: result.id}),
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+  }
+}
+
+export function makeErrorResponse(statusCode: number, message: string): APIGatewayProxyResult {
+  return {
+    statusCode,
+    body: JSON.stringify({ error: message }),
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+  }
+}
 
 export function getCorrelationIdFromHttpRequest(event: APIGatewayProxyEvent): string | undefined {
   return event?.headers?.['x-correlation-id'] as string ?? undefined
