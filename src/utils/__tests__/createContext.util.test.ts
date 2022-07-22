21a971f94086767a13cec80589559368b7a581a6
JBSWE <jackburke1993@gmail.com>
2022-07-22 18:47:11 +0200

    feat: add createContext function for sqs event

--- src/utils/__tests__/createContext.util.test.ts
+++ src/utils/__tests__/createContext.util.test.ts
@@ -1,17 +1,21 @@
-import type { APIGatewayProxyEvent } from 'aws-lambda'
+import type { APIGatewayProxyEvent, SQSEvent } from 'aws-lambda'
 
+import type { SqsRequest, Timer } from '../../models'
+import { Command } from '../../models'
 import {
-  createContextForHttpRequest,
+  createContextForHttpRequest, createContextForSqsEvent,
 } from '../createContext.util'
 
+const lambdaContext = {
+  authorizer: {
+    userRoles: ['test'],
+  },
+  awsRequestId: 'test',
+}
+
 describe('createContext', () => {
+  describe('createContextForHttpRequest', () => {
     it('should return context with corrId from http header', () => {
-      const lambdaContext = {
-        authorizer: {
-          userRoles: ['test'],
-        },
-        awsRequestId: 'test',
-      }
       const httpRequest = {
         headers: {
           'x-correlation-id': 'some-upstream-service-uuid',
@@ -22,4 +26,51 @@ describe('createContext', () => {
 
       expect(context.correlationId).toContain('some-upstream-service-uuid')
     })
+  })
+
+  describe('createContextForSqsEvent', () => {
+    function createSqsEvent(event: SqsRequest): SQSEvent {
+      return {
+        Records: [
+          {
+            body: JSON.stringify(event),
+          },
+        ],
+      } as SQSEvent
+    }
+
+    it('should return default context if meta information is not set in sqs event', () => {
+      const sqsEvent = createSqsEvent({
+        data: {
+          hours: '4',
+          minutes: '0',
+          seconds: '1',
+          url: 'https://someserver.com'
+        } as unknown as Timer,
+      })
+
+      const context = createContextForSqsEvent(sqsEvent, lambdaContext)
+
+      expect(context.correlationId).toContain('timer-api')
+    })
+
+    it('should return context with meta information', () => {
+      const sqsEvent = createSqsEvent({
+        data: {
+          hours: '4',
+          minutes: '0',
+          seconds: '1',
+          url: 'https://someserver.com'
+        } as unknown as Timer,
+        meta: {
+          command: Command.APPLY,
+          correlationId: 'some-upstream-service-uuid',
+        },
+      })
+
+      const context = createContextForSqsEvent(sqsEvent, lambdaContext)
+
+      expect(context.correlationId).toContain('some-upstream-service-uuid')
+    })
+  })
 })
