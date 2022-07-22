772f529d597617e75446b282fe85667515dfa476
JBSWE <jackburke1993@gmail.com>
2022-07-22 19:05:56 +0200

    feat: add time utility to get difference in two times

---
+++ src/utils/time.util.ts
@@ -0,0 +1,12 @@
+import { DateTime, Interval } from 'luxon'
+
+import type { Timer } from '../models'
+
+export function getTimeDifferenceFromNow(timer: Timer): number {
+  const now = DateTime.now().toUTC()
+  const timerExecutionTime = DateTime.fromISO(timer.time)
+
+  const difference = Interval.fromDateTimes(now, timerExecutionTime)
+
+  return Math.floor(difference.length('seconds')) || 0
+}
