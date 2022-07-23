8cdc6758785f798505805e8dcf92ea98cb84e801
JBSWE <jackburke1993@gmail.com>
2022-07-23 12:20:06 +0200

    feat: add commandHandler

---
+++ src/__mocks__/axios.ts
@@ -0,0 +1,3 @@
+import mockAxios from 'jest-mock-axios'
+// eslint-disable-next-line
+export default mockAxios
--- src/handlers/commandHandler.ts
+++ src/handlers/commandHandler.ts
@@ -1,10 +1,77 @@
 import type { SQSEvent } from 'aws-lambda'
+import axios from 'axios'
 
-import type { LambdaRequestContext } from '../models'
+import { SqsPublisher } from '../AWS/sqsPublisher'
+import type { ProcessEnv } from '../config'
+import { getString } from '../config'
+import { TimerDao } from '../dao/timer.dao'
+import type { LambdaRequestContext, SqsRequest, Timer, TimerContext } from '../models'
+import { Command } from '../models'
+import { TimerService } from '../services/timer.service'
+import { createContextForSqsEvent } from '../utils/createContext.util'
+import { getTimeDifferenceFromNow } from '../utils/time.util'
 
-export async function processCommand(event: SQSEvent, context: LambdaRequestContext): Promise<boolean> {
-  if(!event || !context) {
-    return false
+const timerDao = new TimerDao(getString('DYNAMODB_TABLE' as ProcessEnv))
+const sqsPublisher = new SqsPublisher(getString('SQS_URL' as ProcessEnv))
+const timerService = new TimerService(timerDao)
+
+export async function processCommand(event: SQSEvent, context: LambdaRequestContext): Promise<void> {
+  const timerContext = createContextForSqsEvent(event, context)
+
+  const { command, timer } = extractSqsEventDetails(event)
+
+  const timerUrl = `${timer.url}/${timer.id}`
+  const timeDifferenceFromNow = getTimeDifferenceFromNow(timer)
+
+  switch (command) {
+    case Command.APPLY:
+      timerContext.logger.info(`APPLY command for Timer ${timer.id}`)
+      await postUrl(timerUrl, timerContext)
+      break
+    case Command.UPSERT:
+      timerContext.logger.info(`UPSERT command for Timer ${timer.id}`)
+      await processUpsert(timeDifferenceFromNow, timer, timerService, timerContext)
+      break
+    default:
+      timerContext.logger.info('There was no assigned command')
+      break
   }
-  return true
+}
+
+function extractSqsEventDetails(event: SQSEvent): { command: Command | undefined, timer: Timer } {
+  const record = event.Records[0]
+  const message = JSON.parse(record.body) as SqsRequest
+
+  const command = message.meta?.command
+  const timer = message.data as Timer
+
+  return { command, timer }
+}
+
+async function processUpsert(timeDifferenceFromNow: number, timer: Timer, service: TimerService, timerContext: TimerContext): Promise<void> {
+  const FIFTEEN_MINUTES = 900
+  if (timeDifferenceFromNow <= FIFTEEN_MINUTES) {
+    const updatedTimer = {
+      id: timer.id,
+      time: timer.time,
+      url: timer.url,
+      processed: true
+    }
+    await Promise.all([
+      timerService.upsertTimer(timerContext, updatedTimer),
+      sqsPublisher.publish(timerContext, Command.APPLY, updatedTimer, timeDifferenceFromNow)
+    ])
+  } else {
+    await timerService.upsertTimer(timerContext, timer)
+  }
+}
+
+async function postUrl(url: string, timerContext: TimerContext): Promise<void> {
+  try {
+    await axios.post(url)
+    timerContext.logger.info(`Post has successfully been called for url ${url}`)
+  } catch (e) {
+    const error = e as Error
+    timerContext.logger.warn(`Cannot post to url: ${url}`, error.message)
+  }
 }
