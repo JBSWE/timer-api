3719df80d933e1d0b12d8a1552e45bf478ea3a38
JBSWE <jackburke1993@gmail.com>
2022-07-23 13:39:21 +0200

    feat: create http 200 response

--- src/utils/http.util.ts
+++ src/utils/http.util.ts
@@ -2,6 +2,21 @@ import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
 import type { Timer } from '../models'
 
+export function makeOkHttpResponse(result: Timer, timeLeft: number): APIGatewayProxyResult {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      id: result.id,
+      time_left: timeLeft
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+  }
+}
+
 export function makeAcceptedHttpResponse(result: Timer): APIGatewayProxyResult {
   return {
     statusCode: 202,
